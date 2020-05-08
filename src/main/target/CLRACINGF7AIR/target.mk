7ddd849e120ee24de42a002c2b3d20caaaff9ab4
Cheng Lin <bnn1044@gmail.com>
2020-05-08 09:03:24 -0400

    update target file as BF CLRACINGF7 Hardware layout

--- src/main/target/CLRACINGF7AIR/target.mk
+++ src/main/target/CLRACINGF7AIR/target.mk
@@ -1,7 +1,8 @@
-F7X2RE_TARGETS  += $(TARGET)
-FEATURES        +=  VCP ONBOARDFLASH MSC
+F7X2RE_TARGETS += $(TARGET)
+FEATURES        += VCP ONBOARDFLASH MSC
+
 TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6000.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
