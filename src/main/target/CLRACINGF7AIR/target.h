848fd223de22996b393455c85ea61a4e39fc1a79
Cheng Lin <bnn1044@gmail.com>
2020-05-08 09:15:21 -0400

    update target file as BF CLRACINGF7 Hardware layout

--- src/main/target/CLRACINGF7AIR/target.h
+++ src/main/target/CLRACINGF7AIR/target.h
@@ -39,7 +39,7 @@
 #define GYRO_INT_EXTI            PC4
 #define USE_MPU_DATA_READY_SIGNAL
 #define USE_GYRO
-#define USE_GYRO_MPU6500
+#define USE_GYRO_MPU6000
 #define GYRO_MPU6000_ALIGN      CW90_DEG
 #define USE_ACC
 #define USE_ACC_MPU6000
